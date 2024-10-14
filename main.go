905df2f96da295f654d4c6ed869495a6d80f0e2c
tohrxyz <tomas.hrib@proton.me>
2024-10-14 15:43:29 +0200

    init subscribe and read from relay

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/nbd-wtf/go-nostr"
+	"github.com/nbd-wtf/go-nostr/nip19"
+)
+
+func main() {
+	ctx := context.Background()
+
+	relayUrl := "wss://relay.damus.io"
+	relay, err := nostr.RelayConnect(ctx, relayUrl)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Relay url: ", relay.URL)
+
+	npub := "npub"
+	fmt.Println("Npub: ", npub)
+	var filters nostr.Filters
+	if _, v, err := nip19.Decode(npub); err == nil {
+		pub := v.(string)
+		filters = []nostr.Filter{{
+			Kinds:   []int{nostr.KindTimeCalendarEvent},
+			Authors: []string{pub},
+		}}
+	} else {
+		panic(err)
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+
+	sub, err := relay.Subscribe(ctx, filters)
+	if err != nil {
+		panic(err)
+	}
+	for ev := range sub.Events {
+		json, err := json.MarshalIndent(ev, "", " ")
+		if err != nil {
+			fmt.Printf("Error with parsing event: %v", err)
+		}
+		fmt.Printf("%s\n", string(json))
+		fmt.Println("////////////////////////////")
+	}
+}
