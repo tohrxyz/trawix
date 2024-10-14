588b5ddb97d269c1b5e6a594d363aea9f18f72fa
tohrxyz <tomas.hrib@proton.me>
2024-10-14 16:31:44 +0200

    fetch in goroutine and respond on http server

--- main.go
+++ main.go
@@ -4,13 +4,18 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"net/http"
+	"sync"
 	"time"
 
 	"github.com/nbd-wtf/go-nostr"
 	"github.com/nbd-wtf/go-nostr/nip19"
 )
 
-func main() {
+var events []string
+
+func fetchNotes(globalEvents *[]string, wg *sync.WaitGroup) {
+	defer wg.Done()
 	ctx := context.Background()
 
 	relayUrl := "wss://relay.damus.io"
@@ -40,12 +45,37 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	var events []string
 	for ev := range sub.Events {
 		json, err := json.MarshalIndent(ev, "", " ")
 		if err != nil {
 			fmt.Printf("Error with parsing event: %v", err)
 		}
-		fmt.Printf("%s\n", string(json))
-		fmt.Println("////////////////////////////")
+		events = append(events, string(json))
 	}
+	*globalEvents = append(*globalEvents, events...)
+}
+
+func handleEvents(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	data, err := json.Marshal(events)
+	if err != nil {
+		fmt.Printf("Error serializing json: %v", err)
+	}
+	w.Write([]byte(data))
+}
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go fetchNotes(&events, &wg)
+
+	wg.Wait()
+	fmt.Println(events)
+
+	http.HandleFunc("/events", handleEvents)
+	http.ListenAndServe(":1337", nil)
 }
