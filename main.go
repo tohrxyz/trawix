16676f73235fc6a1c8a803ffdf056649aee127e0
tohrxyz <tomas.hrib@proton.me>
2024-10-14 18:12:52 +0200

    better logging, error handling and duration timing

--- main.go
+++ main.go
@@ -5,26 +5,18 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"os"
-	"sync"
 	"time"
 
 	"github.com/nbd-wtf/go-nostr"
 	"github.com/nbd-wtf/go-nostr/nip19"
 )
 
-var (
-	calendarEvents []string
-	mu             sync.Mutex
-)
-
-func fetchNotes(npub string) {
+func fetchNotesOnDemand(npub string, relayUrl string) ([]string, error) {
 	ctx := context.Background()
 
-	relayUrl := "wss://relay.damus.io"
 	relay, err := nostr.RelayConnect(ctx, relayUrl)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	var filters nostr.Filters
 	if _, v, err := nip19.Decode(npub); err == nil {
@@ -34,7 +26,7 @@ func fetchNotes(npub string) {
 			Authors: []string{pub},
 		}}
 	} else {
-		panic(err)
+		return nil, err
 	}
 
 	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
@@ -42,7 +34,7 @@ func fetchNotes(npub string) {
 
 	sub, err := relay.Subscribe(ctx, filters)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	var events []string
@@ -53,41 +45,47 @@ func fetchNotes(npub string) {
 		}
 		events = append(events, string(json))
 	}
-	mu.Lock()
-	calendarEvents = events
-	mu.Unlock()
+	return events, nil
 }
 
-func startPeriodicFetch(interval time.Duration, npub string) {
-	timezone := time.FixedZone("GMT+2", 2*60*60)
-	ticker := time.NewTicker(interval)
-	defer ticker.Stop()
-	for {
-		currentTime := time.Now().In(timezone)
-		fmt.Printf("Refreshing events cache @ %s\n", currentTime.Format("2006-01-02 15:04:05"))
-		<-ticker.C
-		fetchNotes(npub)
+func handleEventsByNpubAndRelay(w http.ResponseWriter, r *http.Request) {
+	timeStart := time.Now()
+	queries := r.URL.Query()
+	npub := queries.Get("npub")
+	if npub == "" {
+		fmt.Printf("No npub found.")
+		http.Error(w, "No npub found", http.StatusBadRequest)
+		return
 	}
-}
+	relay := queries.Get("relay")
+	if relay == "" {
+		fmt.Printf("No relay found.")
+		http.Error(w, "No relay found", http.StatusBadRequest)
+		return
+	}
 
-func handleEvents(w http.ResponseWriter, r *http.Request) {
-	mu.Lock()
-	defer mu.Unlock()
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	data, err := json.Marshal(calendarEvents)
+	events, err := fetchNotesOnDemand(npub, relay)
+	if err != nil {
+		fmt.Printf("Error while fetching notes: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	data, err := json.Marshal(events)
 	if err != nil {
 		fmt.Printf("Error serializing json: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+
+	timeEnd := time.Now()
+	diff := timeEnd.UnixMilli() - timeStart.UnixMilli()
+	sliceNpub := npub[:8] + "..." + npub[len(npub)-8:]
+	fmt.Printf("Success for %s from %s -> took %vms\n", sliceNpub, relay, diff)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(data))
 }
 
 func main() {
-	npub := os.Args[1]
-	go fetchNotes(npub)
-	go startPeriodicFetch(60*time.Second, npub)
-
-	http.HandleFunc("/events", handleEvents)
+	http.HandleFunc("/events", handleEventsByNpubAndRelay)
 
 	fmt.Println("Starting listening on :1337")
 	if err := http.ListenAndServe(":1337", nil); err != nil {
